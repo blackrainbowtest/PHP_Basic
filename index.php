3f469260150debb7567bfcd9e7be36f046077ee1
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-05-11 17:13:27 +0400

    simple registration login form php

--- index.php
+++ index.php
@@ -11,11 +11,14 @@
 <body>
     <form action="login.php" method="post">
         <h2>LOGIN</h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
         <label for="">User Name</label>
         <input type="text" name="uname" placeholder="User Name"><br>
 
         <label for="">User Name</label>
-        <input type="password" name="uname" password="Password"><br>
+        <input type="password" name="password" placeholder="Password"><br>
 
         <button type="submit">Login</button>
     </form>
